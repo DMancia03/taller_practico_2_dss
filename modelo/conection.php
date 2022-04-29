41dcc0717d03a3697e6878071078446b30086462
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2022-04-28 18:36:33 -0600

    Conection add

---
+++ modelo/conection.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conection{
+
+    public function _getConection(){
+        //SERVIDOR LOCALHOST
+        $user = "root";
+        $pass = "";
+        $host = "localhost";
+        $db = "calendar_taller";
+        
+        try{
+            $conectar = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+            //echo "Conexion lograda"; 
+        }catch(PDOException $e){
+            echo $e->getMessage(); 
+        }
+        
+        
+        return $conectar;
+    }
+}
+
+?>
